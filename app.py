e17daea13acea2d2960441caf89f36a54ffc95b1
edward0329 <edward19990329@gmail.com>
2020-03-03 00:36:44 +0800

    make it better

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-app = Flask(__name__)
-
-# Channel Access Token
-line_bot_api = LineBotApi('Nwjh3lECegTpMWwZfR2FVFZG4YWdjHb/2IAANyZrtH3a+Vu3+EbXLY+eAm69DyTZZbmiL9A9dpFY0oKao498sDtUOMZdsslsiWYoKa3UTEytkLmsq49Z5jdIs9KrdNZ3TwhlGapC5aWbw/L1NPCj4QdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
-handler = WebhookHandler('ccdcdf6b37f5f796daaec324d1ef6d99')
-
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -46,10 +29,5 @@ def handle_message(event):
         reply_text = "叫我嗎"
     else:
         reply_text = text
-    message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text)
     line_bot_api.reply_message(event.reply_token, message)
-
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)

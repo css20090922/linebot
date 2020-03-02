f3d64027c15ec02cf1a4b4a2ac33274dbb627739
edward0329 <edward19990329@gmail.com>
2020-03-03 01:18:56 +0800

    make it better

--- app.py
+++ app.py
@@ -5,36 +5,35 @@ handler =util.handler
 
 # 監聽所有來自 /callback 的 Post Request
 
-    @app.route("/callback", methods=['POST'])
-    def callback():
-        # get X-Line-Signature header value
-        signature = request.headers['X-Line-Signature']
-        # get request body as text
-        body = request.get_data(as_text=True)
-        app.logger.info("Request body: " + body)
-        # handle webhook body
-        try:
-            handler.handle(body, signature)
-        except InvalidSignatureError:
-            abort(400)
-        return 'OK'
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
 # 處理訊息
-def handle(handler):
-    @handler.add(MessageEvent, message=TextMessage)
-    def handle_message(event):
-        print(event)
-        text=event.message.text
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print(event)
+    text=event.message.text
 
-        if (text=="Hi"):
-            reply_text = "Hello"
-            #Your user ID
+    if (text=="Hi"):
+        reply_text = "Hello"
+        #Your user ID
 
-        elif(text=="你好"):
-            reply_text = "哈囉"
-        elif(text=="機器人"):
-            reply_text = "叫我嗎"
-        else:
-            reply_text = text
-        message = TextSendMessage(text)
-        line_bot_api.reply_message(event.reply_token, message)
+    elif(text=="你好"):
+        reply_text = "哈囉"
+    elif(text=="機器人"):
+        reply_text = "叫我嗎"
+    else:
+        reply_text = text
+    message = TextSendMessage(text)
+    line_bot_api.reply_message(event.reply_token, message)

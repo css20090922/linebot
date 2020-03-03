ea3a6d51aa041497efe72d30724d464d755c24c5
edward0329 <edward19990329@gmail.com>
2020-03-03 13:10:31 +0800

    make it better

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
-
+print("line secret  "+ channel_secret)
 if channel_secret is None:
     print("Specify LINE_CHANNEL_SECRET as environment variable.")
     sys.exit(1)

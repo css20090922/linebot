a726cef51f7129c1e8ac74681985a934224f642e
edward0329 <edward19990329@gmail.com>
2020-03-03 09:56:22 +0800

    make it better

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import sys
 
 
 from flask import Flask, jsonify, request, abort, send_file
-from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage

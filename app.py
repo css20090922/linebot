324d4a3e29cc89146d1d3f6b4f1069c0d9e1a72d
edward0329 <edward19990329@gmail.com>
2020-03-03 22:18:31 +0800

    make it better

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def handle_message(event):
                   
             ])
         template_message = TemplateSendMessage(
-        alt_text='請用手機看此訊息！', template=buttons_template)
+            alt_text='請用手機看此訊息！', template=buttons_template)
         line_bot_api.reply_message(event.reply_token, buttons_template)
         
         print("PostbackAction")

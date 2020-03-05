9cbc8ec01e8c3d57b37a4d0260e70351d8c5e67e
edward0329 <edward19990329@gmail.com>
2020-03-06 00:06:04 +0800

    make it better

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def isword(word) :
 
 def is_all_chinese(strs):
     for _char in strs:
-        if (not '\u4e00' <= _char <= '\u9fa5') or  _char != '\uff1b' or  _char !=';':
+        if (not '\u4e00' <= _char <= '\u9fa5') or  _char != '；' or  _char !=';':
             return False
     return True
 

f2d9df960d91a56b8cace7979d6f148bcee763df
edward0329 <edward19990329@gmail.com>
2020-03-06 15:54:14 +0800

    make it better

--- app.py
+++ app.py
@@ -103,7 +103,9 @@ def handle_message(event):
             reply_text = "輸入\"小幫手\"可以叫出小幫手\n直接輸入可以查詢單字\n中英文皆可"
         else:
             print(text.lower())
-            reply_text = search_word(text.lower(),language)
+            res = search_word(text.lower(),language)
+            for i in  res:
+                reply_text +=  res[i]
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
     

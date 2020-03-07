7c447bbd466b75397924386bf05e70c80ec9ba93
edward0329 <edward19990329@gmail.com>
2020-03-07 21:12:57 +0800

    result debug

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def handle_message(event):
             reply_text = None
             print(text.lower())
             res = search_word(text.lower(),language)
-            if len(res)>1:
+            if reply_text!="查無此字":
                 reply_text = ';'.join(res)
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)

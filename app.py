d5f6ba2f64f24fee2d48c1404b06281df11197f4
edward0329 <edward19990329@gmail.com>
2020-03-07 21:41:02 +0800

    make it better

--- app.py
+++ app.py
@@ -125,7 +125,10 @@ def handle_message(event):
             print(text.lower())
             res = search_word(text.lower(),language)
             for reply in range(len(res)):
-                reply_text = reply_text + " " + res[reply]
+                if res[reply] == " ":
+                    reply_text +=  "；" 
+                else:
+                    reply_text +=  res[reply]
             
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
